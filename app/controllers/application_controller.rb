c01328925c69ab588c4eecf2922fcdbec75e04e0
cwen-booth <cwen@chicagobooth.edu>
2023-02-01 07:02:17 +0000

    All items complete for now; APIs later

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,79 @@
 class ApplicationController < ActionController::Base
+  def add
+    render({ :template => "/add.html.erb" })
+  end
 
+  def add_results
+    @n1 = params["n1"].to_f
+    @n2 = params["n2"].to_f
+    render({ :template => "/add_results.html.erb" })
+  end
+
+  def subtract
+    render({ :template => "/subtract.html.erb" })
+  end
+
+  def subtract_results
+    @n1 = params["n1"].to_f
+    @n2 = params["n2"].to_f
+    render({ :template => "/subtract_results.html.erb" })
+  end
+
+  def multiply
+    render({ :template => "multiply.html.erb" })
+  end
+
+  def multiply_results
+    @n1 = params["n1"].to_f
+    @n2 = params["n2"].to_f
+    render({ :template => "multiply_results.html.erb" })
+  end
+
+  def divide
+    render({ :template => "divide.html.erb" })
+  end
+
+  def divide_results
+    @n1 = params["n1"].to_f
+    @n2 = params["n2"].to_f
+    render({ :template => "divide_results.html.erb" })
+  end
+
+  def street_to_coords
+    render({ :template => "street_to_coords.html.erb" })
+  end
+
+  def street_to_coords_results
+    @address = params["address"]
+    render({ :template => "street_to_coords_results.html.erb" })
+  end
+
+  def coords_to_weather
+    render({ :template => "coords_to_weather.html.erb" })
+  end
+
+  def coords_to_weather_results
+    @n1 = params["n1"]
+    @n2 = params["n2"]
+    render({ :template => "coords_to_weather_results.html.erb" })
+  end
+
+  def street_to_weather
+    render({ :template => "street_to_weather.html.erb" })
+  end
+
+  def street_to_weather_results
+    @n1 = params["n1"]
+    @n2 = params["n2"]
+    render({ :template => "street_to_weather_results.html.erb" })
+  end
+
+  def translate
+    render({ :template => "translate.html.erb" })
+  end
+
+  def translate_results
+    @text = params["text"]
+    render({ :template => "translate_results.html.erb" })
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,21 @@
 Rails.application.routes.draw do
-
+  get("/", { :controller => "application", :action => "add" })
+  get("/add", { :controller => "application", :action => "add" })
+  get("/wizard_add", { :controller => "application", :action => "add_results" })
+  get("/subtract", { :controller => "application", :action => "subtract" })
+  get("/wizard_subtract", { :controller => "application", :action => "subtract_results" })
+  get("/multiply", { :controller => "application", :action => "multiply" })
+  get("/wizard_multiply", { :controller => "application", :action => "multiply_results" })
+  get("/divide", { :controller => "application", :action => "divide" })
+  get("/wizard_divide", { :controller => "application", :action => "divide_results" })
+  get("/street_to_coords/new", { :controller => "application", :action => "street_to_coords" })
+  get("/street_to_coords/results", { :controller => "application", :action => "street_to_coords_results" })
+  get("/coords_to_weather/new", { :controller => "application", :action => "coords_to_weather" })
+  get("/coords_to_weather/results", { :controller => "application", :action => "coords_to_weather_results" })
+  get("/street_to_weather/new", { :controller => "application", :action => "street_to_weather" })
+  get("/street_to_weather/results", { :controller => "application", :action => "street_to_weather_results" })
+  get("/translate", { :controller => "application", :application => "translate" })
+  get("/translate/results", { :controller => "application", :application => "translate_results" })
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
+\
